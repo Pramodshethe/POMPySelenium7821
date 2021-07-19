ad986df56e38c2b1d7203c83ea9d81b3d54dae0b
Pramodkumar Shethe <Pramodkumar.Shethe@evryindia.in>
2021-07-19 12:25:45 +0530

    added requirement.txt file

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -7,9 +7,8 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def click_on_ele(self,locater):
-         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locater)).click()
+    def click_on_ele(self, locater):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locater)).click()
 
     def send_keys_on_ele(self, locater, text):
-         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locater)).send_keys(text)
-
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locater)).send_keys(text)
